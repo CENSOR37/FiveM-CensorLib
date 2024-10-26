094900cffd4da39684729ef3dfe31cebbe4aa9d2
CENSOR_1337 <viixxivvimm@gmail.com>
2024-10-26 07:23:28 +0700

    feat: add timer component

---
+++ src/imports/components/timer/shared.lua
@@ -0,0 +1,45 @@
+local wait = Wait
+local citizen_create_thread_now = Citizen.CreateThreadNow
+
+local timer = {}
+timer.__index = timer
+function timer.new(handler, delay, is_loop)
+    local self = {}
+
+    self.delay = delay or 0
+    self.is_destroyed = false
+    self.is_loop = (is_loop ~= nil) and is_loop or false
+    self.fn_handler = handler
+
+    self.handler = function()
+        wait(self.delay)
+        if (self.is_destroyed) then return end
+        self.fn_handler()
+    end
+
+    citizen_create_thread_now(function(ref)
+        self.id = ref
+        if (self.is_loop) then
+            while not (self.is_destroyed) do
+                self.handler()
+            end
+        else
+            self.handler()
+        end
+    end)
+
+    return setmetatable(self, timer)
+end
+
+function timer:destroy()
+    if (self.is_destroyed) then return end
+    self.is_destroyed = true
+end
+
+cslib_component = setmetatable({
+    new = timer.new,
+}, {
+    __call = function(_, ...)
+        return timer.new(...)
+    end,
+})
--- src/imports/index.lua
+++ src/imports/index.lua
@@ -5,3 +5,19 @@ local is_server = IsDuplicityVersion()
 
 lib.is_server = is_server
 lib.is_client = not is_server
+
+lib.set_interval = function(handler, delay)
+    return lib.timer.new(handler, delay, true)
+end
+
+lib.set_timeout = function(handler, delay)
+    return lib.timer.new(handler, delay, false)
+end
+
+lib.on_tick = function(handler)
+    return lib.timer.new(handler, 0, true)
+end
+
+lib.on_next_tick = function(handler)
+    return lib.timer.new(handler, 0, false)
+end
