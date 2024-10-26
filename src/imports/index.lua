1f863da84f36d1472801fdb35f325634363549ac
CENSOR_1337 <viixxivvimm@gmail.com>
2024-10-26 08:33:09 +0700

    feat: add common function

--- src/imports/index.lua
+++ src/imports/index.lua
@@ -1,10 +1,32 @@
 --[[ Init ]]
 if (not _VERSION:find("5.4")) then error("^1[ Please enable Lua 5.4 ]^0", 2) end
 
-local is_server = IsDuplicityVersion()
+local native = {
+    register_net_event = RegisterNetEvent,
+    add_event_handler = AddEventHandler,
+    remove_event_handler = RemoveEventHandler,
+    trigger_event = TriggerEvent,
+    is_duplicity_version = IsDuplicityVersion,
+}
+local is_server = native.is_duplicity_version()
+
+local function bind_once(is_network, eventname, listener)
+    local event
+    local fn = function(...)
+        lib.off(event)
+        listener(...)
+    end
+
+    event = is_network and native.register_net_event(eventname, fn) or native.add_event_handler(eventname, fn)
+
+    return event
+end
+
 
 lib.is_server = is_server
 lib.is_client = not is_server
+lib.service = is_server and "server" or "client"
+lib.service_inversed = is_server and "client" or "server"
 
 lib.set_interval = function(handler, delay)
     return lib.timer.new(handler, delay, true)
@@ -21,3 +43,10 @@ end
 lib.on_next_tick = function(handler)
     return lib.timer.new(handler, 0, false)
 end
+
+lib.on = native.add_event_handler
+lib.off = native.remove_event_handler
+lib.emit = native.trigger_event
+lib.once = function(eventname, listener) return bind_once(false, eventname, listener) end
+lib[("on_%s"):format(lib.service_inversed)] = native.register_net_event
+lib[("once_%s"):format(lib.service_inversed)] = function(eventname, listener) return bind_once(true, eventname, listener) end
