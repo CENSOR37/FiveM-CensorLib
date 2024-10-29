e95adc6919e4f0784d60d5e32df6ef1f048099fa
CENSOR_1337 <viixxivvimm@gmail.com>
2024-10-30 06:54:14 +0700

    feat: add chance_pool

--- src/imports/components/random/shared.lua
+++ src/imports/components/random/shared.lua
@@ -75,3 +75,63 @@ end
 
 cslib_component.string = setmetatable({ new = randomize_string.random }, { __call = function(_, ...) return randomize_string.random(...) end })
 cslib_component.uuid = setmetatable({ new = uuid.random }, { __call = function(_, ...) return uuid.random() end })
+
+local chance_pool = {}
+chance_pool.__index = chance_pool
+
+function chance_pool.new()
+    local self = setmetatable({}, chance_pool)
+    self.pool = {}
+    self.key = 10
+    self.cumulative = 0
+    return self
+end
+
+function chance_pool:calculate_cumulative()
+    self.cumulative = 0
+
+    for _, item in pairs(self.pool) do
+        self.cumulative = self.cumulative + item.chance
+        item.chance_end = self.cumulative
+    end
+end
+
+function chance_pool:add_item(chance, data)
+    lib.validate.type.assert(chance, "number")
+
+    if not (data) then
+        error("data is required")
+    end
+
+    self.key = self.key + 1
+    self.pool[self.key] = { chance = chance, data = data }
+
+    self:calculate_cumulative()
+
+    return self.key
+end
+
+function chance_pool:remove_item(key)
+    lib.validate.type.assert(key, "number")
+
+    self.pool[key] = nil
+    self:calculate_cumulative()
+end
+
+function chance_pool:random()
+    if (self.cumulative == 0) then return nil end
+
+    local random = math.random() * self.cumulative
+
+    for _, value in pairs(self.pool) do
+        if (random <= value.chance_end) then
+            return value.data
+        end
+    end
+end
+
+cslib_component.chance_pool = setmetatable({ new = chance_pool.new }, {
+    __call = function()
+        return chance_pool.new()
+    end,
+})
