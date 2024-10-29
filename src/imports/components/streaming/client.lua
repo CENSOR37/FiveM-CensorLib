e0b53b1743126105456f80a9e7a7f4730fce3e36
CENSOR_1337 <viixxivvimm@gmail.com>
2024-10-30 05:03:28 +0700

    feat: add streaming component

---
+++ src/imports/components/streaming/client.lua
@@ -0,0 +1,76 @@
+local native = {
+    does_anim_dict_exist = DoesAnimDictExist,
+    has_anim_dict_loaded = HasAnimDictLoaded,
+    request_anim_dict = RequestAnimDict,
+    remove_anim_dict = RemoveAnimDict,
+    is_model_valid = IsModelValid,
+    has_model_loaded = HasModelLoaded,
+    request_model = RequestModel,
+    set_model_as_no_longer_needed = SetModelAsNoLongerNeeded,
+    citizen_wait = Citizen.Wait,
+}
+
+local function request_animdict(in_animdict)
+    lib.validate.type.assert(in_animdict, "string")
+
+    if (not native.does_anim_dict_exist(in_animdict)) then
+        error(("animdict \"%s\" was not exist"):format(in_animdict))
+    end
+
+    if (native.has_anim_dict_loaded(in_animdict)) then
+        return true
+    end
+
+    native.request_anim_dict(in_animdict)
+    while native.has_anim_dict_loaded(in_animdict) do
+        native.citizen_wait(0)
+    end
+
+    return true
+end
+
+local function request_model(in_model)
+    lib.validate.type.assert(in_model, "string", "number")
+
+    local model_hash = type(in_model) == "number" and in_model or joaat(in_model)
+
+    if (not native.is_model_valid(model_hash)) then
+        error(("model \"%s\" is not valid"):format(in_model))
+    end
+
+    if (native.has_model_loaded(model_hash)) then
+        return true
+    end
+
+    native.request_model(model_hash)
+    while native.has_model_loaded(model_hash) do
+        native.citizen_wait(0)
+    end
+
+    return true
+end
+
+local warpped_request_anim_dict = lib.promise.warp_fn(request_animdict)
+local warpped_request_model = lib.promise.warp_fn(request_model)
+
+cslib_component.animdict = {
+    request = setmetatable({}, {
+        __call = function(_, ...)
+            return warpped_request_anim_dict(...)
+        end,
+    }),
+    clear = native.remove_anim_dict,
+    has_loaded = native.has_anim_dict_loaded,
+    is_valid = native.does_anim_dict_exist,
+}
+
+cslib_component.model = {
+    request = setmetatable({}, {
+        __call = function(_, ...)
+            return warpped_request_model(...)
+        end,
+    }),
+    clear = native.set_model_as_no_longer_needed,
+    has_loaded = native.has_model_loaded,
+    is_valid = native.is_model_valid,
+}
