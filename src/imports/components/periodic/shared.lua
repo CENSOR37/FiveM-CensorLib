9c4d55a458a916e05b4b3444abd3e1b37aa30f94
CENSOR_1337 <viixxivvimm@gmail.com>
2024-11-09 05:48:15 +0700

    feat: add `periodic` component

---
+++ src/imports/components/periodic/shared.lua
@@ -0,0 +1,74 @@
+local periodic = {}
+periodic.__index = periodic
+
+function periodic.new(opts)
+    opts = opts or {}
+    local self = {}
+    self.handlers = {
+        fn = {},
+        list = {},
+        length = 0,
+    }
+    self.b_reassign_table = false
+    self.id = 10
+    self.tick_rate = opts.tick_rate or 0
+    self.interval = nil
+
+    return setmetatable(self, periodic)
+end
+
+function periodic:add(fn_handler)
+    lib.validate.type.assert(fn_handler, "function")
+
+    self.id += 1
+    self.handlers.fn[self.id] = fn_handler
+    self.b_reassign_table = true
+
+    if not (self.interval) then
+        self.interval = lib.set_interval(function()
+            local entries = self.handlers.list
+
+            if (self.b_reassign_table) then
+                table.wipe(entries)
+                for _, value in pairs(self.handlers.fn) do
+                    entries[#entries + 1] = value
+                end
+                self.handlers.length = #entries
+                if (self.handlers.length <= 0) then
+                    self.interval:destroy()
+                    self.interval = nil
+                end
+            end
+
+            for i = 1, self.handlers.length, 1 do
+                entries[i]()
+            end
+        end, self.tick_rate)
+    end
+
+    return self.id
+end
+
+function periodic:remove(id)
+    lib.validate.type.assert(id, "number")
+
+    self.handlers.fn[id] = nil
+    self.b_reassign_table = true
+end
+
+function periodic:clear()
+    table.wipe(self.handlers.fn)
+    self.b_reassign_table = true
+end
+
+function periodic:destroy()
+    self:clear()
+end
+
+lib_module = setmetatable({
+    new = periodic.new,
+}, {
+    __call = function(_, ...)
+        return periodic.new(...)
+    end,
+})
