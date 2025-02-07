cf50d8b7c901fcc4909241c856497fea8ed5211d
CENSOR37 <viixxivvimm@gmail.com>
2025-02-07 12:49:07 +0700

    chore: replace condition error with assert

--- src/imports/index.lua
+++ src/imports/index.lua
@@ -1,5 +1,5 @@
 --[[ Init ]]
-if (not _VERSION:find("5.4")) then error("^1[ Please enable Lua 5.4 ]^0", 2) end
+assert(_VERSION:find("5.4"), "^1[ Please enable Lua 5.4 ]^0")
 
 local native = {
     register_net_event = RegisterNetEvent,
