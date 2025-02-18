ec4f83c3a61f32df30225be65623a577b2021698
CENSOR37 <viixxivvimm@gmail.com>
2025-02-18 10:18:27 +0700

    refactor: change parameter of poly colshape

--- src/imports/components/colshape/shared.lua
+++ src/imports/components/colshape/shared.lua
@@ -3,6 +3,8 @@
 local glm = require "glm"
 local glm_polygon_contains = glm.polygon.contains
 
+local numdeci = function(value) return value + 0.0 end
+
 local native = {
     draw_marker = DrawMarker,
     draw_box = DrawBox,
@@ -10,7 +12,30 @@ local native = {
     get_entity_coords = GetEntityCoords,
     draw_line = DrawLine,
     draw_poly = DrawPoly,
+    world3d_to_screen2d = World3dToScreen2d,
+    set_text_scale = SetTextScale,
+    set_text_colour = SetTextColour,
+    set_text_dropshadow = SetTextDropshadow,
+    set_text_edge = SetTextEdge,
+    set_text_outline = SetTextOutline,
+    set_text_entry = SetTextEntry,
+    add_text_component_string = AddTextComponentString,
+    draw_text = DrawText,
 }
+
+local function draw_text_3d_dbg(text, point)
+    local on_screen, x, y = native.world3d_to_screen2d(point.x, point.y, point.z)
+    if not (on_screen) then return end
+
+    native.set_text_scale(0.0, 0.25)
+    native.set_text_colour(255, 255, 255, 255)
+    native.set_text_dropshadow(0, 0, 0, 0, 255)
+    native.set_text_edge(2, 0, 0, 0, 150)
+    native.set_text_outline()
+    native.set_text_entry("STRING")
+    native.add_text_component_string(text)
+    native.draw_text(x, y)
+end
 
 local function table_map(table, cb)
     local new_table = {}
@@ -186,42 +211,64 @@ local colshape_poly = {}
 colshape_poly.__index = colshape_poly
 setmetatable(colshape_poly, { __index = colshape })
 
-function colshape_poly.new(points, thickness)
-    thickness = thickness or 2
+function colshape_poly.new(in_points, in_min_z, in_max_z)
+    in_min_z = in_min_z or -10000.0
+    in_max_z = in_max_z or 10000.0
 
-    lib.validate.type.assert(points, "table")
-    lib.validate.type.assert(thickness, "number")
+    lib.validate.type.assert(in_points, "table")
+    lib.validate.type.assert(in_min_z, "number")
+    lib.validate.type.assert(in_max_z, "number")
 
     local self = setmetatable(colshape.new(), colshape_poly)
-    self.points = table_map(points, function(point) return vec(point.x, point.y, point.z) end)
-    self.thickness = thickness
+    self.points = {}
+    self.min_z = numdeci(in_min_z)
+    self.max_z = numdeci(in_max_z)
+    local poly_z = (self.min_z + self.max_z) / 2.0
+    for i = 1, #in_points do
+        self.points[i] = vec(in_points[i].x, in_points[i].y, poly_z)
+    end
     self.polygon = glm.polygon.new(self.points)
-    self.triangles = {
-        get_triangles(glm.polygon.new(table_map(self.points, function(point) return point - vec(0, 0, thickness / 2) end))),
-        get_triangles(glm.polygon.new(table_map(self.points, function(point) return point + vec(0, 0, thickness / 2) end))),
-    }
+    self.thickness = (self.max_z - self.min_z) / 4.0
+
     return self
 end
 
 function colshape_poly:is_position_inside(position)
-    return glm_polygon_contains(self.polygon, vec(position.x, position.y, position.z), self.thickness / 4)
+    local point = vec(position.x, position.y, position.z)
+    return glm.polygon.contains(self.polygon, point, self.thickness)
 end
 
 function colshape_poly:draw_debug()
     if (lib.is_server) then return end
 
-    local ped = native.player_ped_id()
-    local coords = native.get_entity_coords(ped)
+    local ped                 = native.player_ped_id()
+    local coords              = native.get_entity_coords(ped)
     local is_local_ped_inside = self:is_position_inside(coords)
-    local color = is_local_ped_inside and { r = 0, g = 255, b = 0, a = 75 } or { r = 0, g = 0, b = 255, a = 75 }
+    local color               = is_local_ped_inside and { r = 0, g = 255, b = 0, a = 75 } or { r = 0, g = 0, b = 255, a = 75 }
+    local points              = self.points
+    local min_z               = self.min_z or -10000.0
+    local top_z               = self.max_z or 10000.0
+    local z_offset            = top_z - min_z
+    local r, g, b, a          = color.r, color.g, color.b, color.a
 
-    if not (self.triangles) then
-        self.triangles = {
-            get_triangles(glm.polygon.new(table_map(self.polygon, function(point) return point - vec(0, 0, self.thickness / 2) end))),
-            get_triangles(glm.polygon.new(table_map(self.polygon, function(point) return point + vec(0, 0, self.thickness / 2) end))),
-        }
+    for i = 1, #points do
+        local current_point = points[i]
+        local next_point    = points[(i % #points) + 1]
+
+        local cx, cy        = current_point.x, current_point.y
+        local nx, ny        = next_point.x, next_point.y
+
+        native.draw_poly(cx, cy, min_z, nx, ny, min_z, cx, cy, min_z + z_offset, r, g, b, a)
+        native.draw_poly(nx, ny, min_z, nx, ny, min_z + z_offset, cx, cy, min_z + z_offset, r, g, b, a)
+        native.draw_poly(cx, cy, min_z + z_offset, nx, ny, min_z + z_offset, cx, cy, min_z, r, g, b, a)
+        native.draw_poly(nx, ny, min_z + z_offset, nx, ny, min_z, cx, cy, min_z, r, g, b, a)
+
+        native.draw_line(cx, cy, min_z, nx, ny, min_z, 255, 0, 0, 255)
+        native.draw_line(cx, cy, min_z + z_offset, nx, ny, min_z + z_offset, 255, 0, 0, 255)
+        native.draw_line(cx, cy, min_z, cx, cy, min_z + z_offset, 255, 0, 0, 255)
+
+        draw_text_3d_dbg(tostring(i), vec(cx, cy, min_z + z_offset))
     end
-    draw_debug_polygon(self.triangles, self.polygon, self.thickness, color)
 end
 
 -- colshape_box
