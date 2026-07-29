58ddc24bdbf81b4c0999d237971ca21448b8c487
CENSOR37 <viixxivvimm@gmail.com>
2026-07-30 03:27:38 +0700

    add adaptive event

--- src/imports/components/_event/shared.lua
+++ src/imports/components/_event/shared.lua
@@ -1,6 +1,23 @@
 local msgpack_pack_args = msgpack.pack_args
 local is_server = IsDuplicityVersion()
 
+-- TODO: make thoes convars
+local LATENT_THRESHOLD <const> = 65536
+local LATENT_TARGET_SEC <const> = 1.0
+local LATENT_MIN_BPS <const> = 50000
+local LATENT_MAX_BPS <const> = 500000
+
+local function calc_bps(payload_len)
+    local bps = math.ceil(payload_len / LATENT_TARGET_SEC)
+    return math.min(math.max(bps, LATENT_MIN_BPS), LATENT_MAX_BPS)
+end
+
+local function pack_payload(...)
+    local payload = msgpack_pack_args(...)
+    local payload_len = #payload
+    return payload, payload_len, payload_len >= LATENT_THRESHOLD
+end
+
 local function add_event_handler(event_name, listener, is_remote)
     if (is_remote) then
         return RegisterNetEvent(event_name, function(...)
@@ -56,6 +73,20 @@ if (is_server) then
         TriggerLatentClientEvent(event_name, target, bps, ...)
     end
 
+    event.emit_client_adaptive = function(event_name, target, ...)
+        assert(type(target) == "number" or (type(target) == "string" and tonumber(target) ~= nil), "Target client ID must be a number or a string that can be converted to a number.")
+        assert(tonumber(target) > 0, "Target client ID must be greater than 0, or use emit_all_clients_adaptive instead.")
+
+        local payload, payload_len, is_latent = pack_payload(...)
+
+        if (is_latent) then
+            local bps = calc_bps(payload_len)
+            TriggerLatentClientEventInternal(event_name, target, payload, payload_len, bps)
+        else
+            TriggerClientEventInternal(event_name, target, payload, payload_len)
+        end
+    end
+
     event.once_client = function(event_name, listener)
         return bind_once(event_name, listener, true)
     end
@@ -66,6 +97,17 @@ if (is_server) then
 
     event.emit_all_clients_latent = function(event_name, bps, ...)
         TriggerLatentClientEvent(event_name, -1, bps, ...)
+    end
+
+    event.emit_all_clients_adaptive = function(event_name, ...)
+        local payload, payload_len, is_latent = pack_payload(...)
+
+        if (is_latent) then
+            local bps = calc_bps(payload_len)
+            TriggerLatentClientEventInternal(event_name, -1, payload, payload_len, bps)
+        else
+            TriggerClientEventInternal(event_name, -1, payload, payload_len)
+        end
     end
 
     event.emit_clients = function(event_name, clients, ...)
@@ -86,10 +128,36 @@ if (is_server) then
             TriggerLatentClientEventInternal(event_name, clients[i], payload, payload_len, bps)
         end
     end
+
+    event.emit_clients_adaptive = function(event_name, clients, ...)
+        local payload, payload_len, is_latent = pack_payload(...)
+
+        if (is_latent) then
+            local bps = calc_bps(payload_len)
+            for i = 1, #clients do
+                TriggerLatentClientEventInternal(event_name, clients[i], payload, payload_len, bps)
+            end
+        else
+            for i = 1, #clients do
+                TriggerClientEventInternal(event_name, clients[i], payload, payload_len)
+            end
+        end
+    end
 else
     event.on_server = add_net_handler
     event.emit_server = TriggerServerEvent
     event.emit_server_latent = TriggerLatentServerEvent
+
+    event.emit_server_adaptive = function(event_name, ...)
+        local payload, payload_len, is_latent = pack_payload(...)
+
+        if (is_latent) then
+            local bps = calc_bps(payload_len)
+            TriggerLatentServerEventInternal(event_name, payload, payload_len, bps)
+        else
+            TriggerServerEventInternal(event_name, payload, payload_len)
+        end
+    end
 
     event.once_server = function(event_name, listener)
         return bind_once(event_name, listener, true)
